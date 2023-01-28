ac3e3a624b5eb15a9ae15e1138f9778f6330d5eb
Amarilio de Oliveira <amarilioalencar@gmail.com>
2023-01-28 15:34:10 -0300

    Subindo o projeto de um site pessoal com efeitos em Parallax.

--- assets/js/img-modal.js
+++ assets/js/img-modal.js
@@ -2,15 +2,82 @@ const images = document.querySelectorAll(".gallery img");
 const modal = document.querySelector(".modal");
 const modalImg = document.querySelector(".modalImg");
 const modalTxt = document.querySelector(".modalTxt");
-const close = document.querySelector(".close");
+const close = document.querySelector(".closeBtn");
+const prevBtn = document.querySelector(".prevBtn");
+const nextBtn = document.querySelector(".nextBtn");
+const body = document.body;
 
-images.forEach((image) => {
+images.forEach((image, index) => {
   image.addEventListener("click", () => {
+    body.classList.add("body-no-scroll");
     modalImg.src = image.src;
     modalTxt.innerHTML = image.alt;
     modal.classList.add("appear");
+    body.classList.add("body-no-scroll");
+
+    let imageIndex = index;
+    let next = imageIndex++;
+    let prev = imageIndex--;
+
+    /* - - - - - - - - - -  */
+    /* Work with Arrow Keys */
+    /* - - - - - - - - - -  */
+    window.addEventListener("keyup", (e) => {
+      if (next >= images.length) {
+        next = 0;
+      }
+      if (prev < 0) {
+        prev = images.length - 1;
+      }
+
+      if (e.keyCode === 37) {
+        modalImg.src = images[prev].src;
+        modalTxt.innerHTML = images[prev].alt;
+        prev--;
+        next = prev + 2;
+      } else if (e.keyCode === 39) {
+        modalImg.src = images[next].src;
+        modalTxt.innerHTML = images[next].alt;
+        next++;
+        prev = next - 2;
+      } else if (e.keyCode === 27) {
+        modal.classList.remove("appear");
+      }
+    });
+
+    /* - - - - - - - - - */
+    /* Work with Buttons */
+    /* - - - - - - - - - */
+    prevBtn.addEventListener("click", () => {
+      modalImg.src = images[prev].src;
+      modalTxt.innerHTML = images[prev].alt;
+      prev--;
+      next = prev + 2;
+
+      if (next >= images.length) {
+        next = 0;
+      }
+      if (prev < 0) {
+        prev = images.length - 1;
+      }
+    });
+
+    nextBtn.addEventListener("click", () => {
+      modalImg.src = images[next].src;
+      modalTxt.innerHTML = images[next].alt;
+      next++;
+      prev = next - 2;
+
+      if (next >= images.length) {
+        next = 0;
+      }
+      if (prev < 0) {
+        prev = images.length - 1;
+      }
+    });
 
     close.addEventListener("click", () => {
+      body.classList.remove("body-no-scroll");
       modal.classList.remove("appear");
     });
   });
