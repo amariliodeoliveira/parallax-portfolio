659d9773921f358d67c455b1eecbf591ecf31544
Amarilio de Oliveira <amarilioalencar@gmail.com>
2023-02-04 02:10:30 -0300

    Changes to about me, removal of unnecessary images and update do README.md.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Parallax
+# Facebook (Login Page)
+Clone of Facebook's login page.
+
+## Technologies used
+<a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank" rel="noopener noreferrer">![HTML5](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)</a>
+<a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank" rel="noopener noreferrer">![CSS3](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)</a>
+
+## Preview screenshot
+![](https://github.com/amariliodeoliveira/parallax-portfolio/assets/img/previewScreenshot.jpg)
--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,9 +1,9 @@
 const stars = document.getElementById("stars");
 const moon = document.getElementById("moon");
-const mountains_behind = document.getElementById("mountains_behind");
-const text = document.getElementById("text");
+const mountains_behind = document.getElementById("mountainsBehind");
+/* const text = document.getElementById("text"); */
 const btn = document.getElementById("btn");
-const mountains_front = document.getElementById("mountains_front");
+const first_layer = document.getElementById("firstLayer");
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", function () {
@@ -11,9 +11,9 @@ window.addEventListener("scroll", function () {
   stars.style.left = value * 0.25 + "px";
   moon.style.top = value * 1.05 + "px";
   mountains_behind.style.top = value * 0.5 + "px";
-  mountains_front.style.top = value * 0 + "px";
-  text.style.marginRight = value * 3.5 + "px";
-  text.style.marginTop = value * 1.5 + "px";
+  first_layer.style.top = value * 0 + "px";
+/*   text.style.marginRight = value * 3.5 + "px";
+  text.style.marginTop = value * 1.5 + "px"; */
   btn.style.marginTop = value * 1.5 + "px";
   header.style.top = value * 0.5 + "px";
 });
